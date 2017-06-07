26bd899d77ba16f5352d478cda18272420b9ee58
BVPKY <b.v.prudhvikumar@gmail.com>
2017-06-07 17:42:52 +0530

    WOIR.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,4 @@
-
-
-
-
-
 package in.woir.http;
-
-
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
---
+++ WOIR.java
@@ -0,0 +1,6 @@
+public class WOIR {
+public void printme( ){
+System.out.println("Hello, World From WOIR");
+}
+}
+
