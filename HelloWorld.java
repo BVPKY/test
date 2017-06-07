fd7ace2cca740e2cf0d4e1987b62573c0555f04a
BVPKY <b.v.prudhvikumar@gmail.com>
2017-06-07 17:23:14 +0530

    HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,28 +1,17 @@
-asdas
-
-asdasdas
-
-
-asdasdfdfds
 
 
 
 
 
+package in.woir.http;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-sdfsdfsdfsdfsdfsd
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+	//WOIR sc =new WOIR();
+//sc.printme();
+}
+   
+}
